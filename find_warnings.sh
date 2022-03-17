ea8b7ca957429082c0a335dd41ce85008e39e0f4
20373822 <20373822.buaa.edu.cn>
2022-03-17 16:29:17 +0800

    1.2

--- find_warnings.sh
+++ find_warnings.sh
@@ -1,5 +1,7 @@
 #!bin/bash
-gcc -Wall $1 -o test 2> warning.txt | sed 's/warning: //g' >> result.txt
+gcc -Wall $1 -o test 2> warning.txt 
+grep warning: warning.txt | sed -n 's/warning: //g' >> result.txt
+gcc -Wall $1 -o test 2> warning.txt
 a=1
 if [ $? -eq 0 ]
 then
