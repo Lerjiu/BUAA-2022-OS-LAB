9fd12b48b38c0dff8a7e849ce114006e455784fc
20373822 <20373822.buaa.edu.cn>
2022-05-12 16:09:34 +0800

    1

--- lib/sched.c
+++ lib/sched.c
@@ -11,16 +11,13 @@
  *  2. Use variable 'env_sched_list', which is a pointer array.
  *  3. CANNOT use `return` statement!
  */
-/*** exercise 3.15 ***/
-
+/*** exercise 3.14 ***/
 void sched_yield(void)
 {
-	printf("\n");
-    static int count = 0; // remaining time slices of current env
-    static int point = 0; // current env_sched_list index
-    static struct Env *e = NULL;
-
-    /*  hint:
+	static int count = 0; // remaining time slices of current env
+	static int point = 0; // current env_sched_list index
+	static struct Env *e = NULL;
+	/*  hint:
      *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
      *     using LIST_REMOVE and LIST_INSERT_TAIL.
      *  2. if (env_sched_list[point] is empty), point = 1 - point;
@@ -32,72 +29,50 @@ void sched_yield(void)
      *  functions or macros below may be used (not all):
      *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
      */
-	if(count == 0 || e == NULL || e->env_status != ENV_RUNNABLE)
+	if (count == 0 || e == NULL || e->env_status != ENV_RUNNABLE)
 	{
-		if(e != NULL)
+		if (e != NULL)
 		{
 			LIST_REMOVE(e, env_sched_link);
-			if(e->env_status != ENV_FREE)
+			if (e->env_status != ENV_FREE)
 			{
-				if(((e->env_pri) % 2) == 1)
-				{
-				//	if(point == 0)
-				//		LIST_INSERT_TAIL(&env_sched_list[1], e, env_sched_link);
-				//	if(point == 1)
-				//		LIST_INSERT_TAIL(&env_sched_list[2], e, env_sched_link);
-				//	if(point == 2)
-				//		LIST_INSERT_TAIL(&env_sched_list[0], e, env_sched_link);
-					LIST_INSERT_TAIL(&env_sched_list[(point+1)%3], e, env_sched_link);
-				}else{
-				//	if(point == 0)
-                //       LIST_INSERT_TAIL(&env_sched_list[2], e, env_sched_link);
-                 //   if(point == 1)
-                   //     LIST_INSERT_TAIL(&env_sched_list[0], e, env_sched_link);
-                 //   if(point == 2)
-                   //     LIST_INSERT_TAIL(&env_sched_list[1], e, env_sched_link);
+				if(((e->env_pri) % 2) == 0)
+                {
 					LIST_INSERT_TAIL(&env_sched_list[(point+2)%3], e, env_sched_link);
-				}
+               
+                }else{
+					LIST_INSERT_TAIL(&env_sched_list[(point+1)%3], e, env_sched_link);
+                }
 			}
 		}
-		
-		while(1)
+		while (1)
 		{
-			while(LIST_EMPTY(&env_sched_list[point]))
-			{				
-			//	if(point == 2){
-			//		point = 0;
-			//	}else{
-			//		point++;
-			//	}
-				point = (point + 1)%3;
+			while (LIST_EMPTY(&env_sched_list[point]))
+			{
+				if(point == 2){
+					point = 0;
+				}else{
+					point = point + 1;
+				}
 			}
-
 			e = LIST_FIRST(&env_sched_list[point]);
-
-			if(e->env_status == ENV_FREE)
+			if (e->env_status == ENV_FREE)
+				LIST_REMOVE(e, env_sched_link);
+			else if (e->env_status == ENV_NOT_RUNNABLE)
 			{
 				LIST_REMOVE(e, env_sched_link);
-			}else if(e->env_status == ENV_NOT_RUNNABLE){
-				LIST_REMOVE(e, env_sched_link);
-				if(((e->env_pri) % 2) == 1)
+				if(((e->env_pri) % 2) == 0)
                 {
-					LIST_INSERT_TAIL(&env_sched_list[(point+1)%3], e, env_sched_link);
-                 //   if(point == 0)
-                 //       LIST_INSERT_TAIL(&env_sched_list[1], e, env_sched_link);
-                 //   if(point == 1)
-                 //       LIST_INSERT_TAIL(&env_sched_list[2], e, env_sched_link);
-                 //   if(point == 2)
-                 //       LIST_INSERT_TAIL(&env_sched_list[0], e, env_sched_link);
-                }else{
-                    /*if(point == 0)
-                        LIST_INSERT_TAIL(&env_sched_list[2], e, env_sched_link);
-                    if(point == 1)
-                        LIST_INSERT_TAIL(&env_sched_list[0], e, env_sched_link);
-                    if(point == 2)
-                        LIST_INSERT_TAIL(&env_sched_list[1], e, env_sched_link);*/
 					LIST_INSERT_TAIL(&env_sched_list[(point+2)%3], e, env_sched_link);
+               
+                }else{
+                  
+					LIST_INSERT_TAIL(&env_sched_list[(point+1)%3], e, env_sched_link);
                 }
-			}else{
+			//	LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
+			}
+			else
+			{
 				if(point == 0)
 				{
 					count = e->env_pri;
@@ -106,15 +81,13 @@ void sched_yield(void)
 				}else{
 					count = e->env_pri * 4;
 				}
+
 				break;
 			}
 		}
 	}
-//	printf("\n");	
 	count--;
-	e->env_runs++;
-//	printf("\n");
+	printf("\n");
 	env_run(e);
-//	env_run(LIST_FIRST(env_sched_list));
-//	printf("\n");
+	//	env_run(LIST_FIRST(env_sched_list));
 }
