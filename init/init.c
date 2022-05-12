294f57aa5e5cbf11508a82c94efdc1d069450330
20373822 <20373822.buaa.edu.cn>
2022-05-12 16:14:40 +0800

    1

--- init/init.c
+++ init/init.c
@@ -4,45 +4,6 @@
 #include <printf.h>
 #include <kclock.h>
 #include <trap.h>
-/*
-void mips_init()
-{
-	u_int *target;
-	target = 0x80000080;
-	printf("before mips_detect_memory: 0x%x\n",*target);
-	printf("init.c:\tmips_init() is called\n");
-	mips_detect_memory();
-	printf("before mips_vm_init: 0x%x\n",*target);
-	mips_vm_init();
-	printf("before page_init: 0x%x\n",*target);
-	page_init();
-	printf("before env_init: 0x%x\n",*target);
-	env_init();
-	printf("before env_check: 0x%x\n",*target);
-	env_check();
-	printf("before load_icode_check: 0x%x\n",*target);
-	load_icode_check();
-
-	/*you can create some processes(env) here. in terms of binary code, please refer current directory/code_a.c
-	 * code_b.c*/
-	/*** exercise 3.9 ***/
-	/*you may want to create process by MACRO, please read env.h file, in which you will find it. this MACRO is very
-	 * interesting, have fun please*/
-
-/*	printf("before create A: 0x%x\n",*target);
-	ENV_CREATE_PRIORITY(user_A, 2);
-	printf("before create B: 0x%x\n",*target);
-	ENV_CREATE_PRIORITY(user_B, 1);
-	printf("before trap_init: 0x%x\n",*target);
-	trap_init();
-	printf("before kclock_init: 0x%x\n",*target);
-	kclock_init();
-	printf("before panic\n");
-	panic("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-	printf("after panic\n");
-	while(1);
-	panic("init.c:\tend of mips_init() reached!");
-}*/
 
 void mips_init()
 {
