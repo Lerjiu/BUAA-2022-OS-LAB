429490fa40049f982f51bdfd1710ab37f09fdd07
李文豪 <1426239779@qq.com>
2022-06-30 15:40:41 +0800

    lab4-challenge

--- init/init.c
+++ init/init.c
@@ -21,7 +21,7 @@ void mips_init() {
 //    ENV_CREATE(user_pingpong);
 //    ENV_CREATE_PRIORITY(user_a,1);
 //    ENV_CREATE_PRIORITY(user_b,2);
-    ENV_CREATE(user_pttest);
+//    ENV_CREATE(user_pttest);
 //    ENV_CREATE(user_ptexittest);
 //    ENV_CREATE(user_ptcanceltest);
 //    ENV_CREATE(user_ptjointest);
--- user/semtest.c
+++ user/semtest.c
@@ -1,64 +1,60 @@
-//
-// Created by 李文豪 on 2022/6/29.
-//
-
 #include "lib.h"
 
-void *run1(void *args) {
-    writef("thread1\n");
-    sem_t *sem = (sem_t *)((u_int *)args)[0];
+void *test1(void *args) {
+    u_int father_thread = ((u_int *)args)[0];
+    sem_t *sem = (sem_t *)((u_int *)args)[1];
     int a;
     int r;
     for (a = 0; a < 2; ++a) {
         r = sem_wait(sem);
         if (r < 0) {
-            user_panic("fail P at thread2: %d\n",r);
+            user_panic("fail P at son1: %d\n",r);
         }
-        writef("thread1 P! now a is %d\n",a + 1);
+        writef("son1 P! now a is %d\n",a);
     }
 }
 
-void *run2(void *args) {
-    writef("thread2\n");
-    sem_t *sem = (sem_t *)((u_int *)args)[0];
+void *test2(void *args) {
+    u_int father_thread = ((u_int *)args)[0];
+    sem_t *sem = (sem_t *)((u_int *)args)[1];
     int b;
     int r;
     for (b = 0; b < 3; ++b) {
         r = sem_wait(sem);
         if (r < 0) {
-            user_panic("fail P at thread2: %d\n",r);
+            user_panic("fail P at son2: %d\n",r);
         }
-        writef("thread2 P! now b is %d\n",b + 1);
+        writef("son2 P! now b is %d\n",b);
     }
 }
 
-void *run3(void *args) {
-    writef("thread3\n");
-    sem_t *sem = (sem_t *)((u_int *)args)[0];
+void *test3(void *args) {
+    u_int father_thread = ((u_int *)args)[0];
+    sem_t *sem = (sem_t *)((u_int *)args)[1];
     int c;
     int r;
     for(c = 0; c < 5; ++c) {
         r = sem_wait(sem);
         if (r < 0) {
-            user_panic("fail P at thread3: %d\n",r);
+            user_panic("fail P at son3: %d\n",r);
         }
-        writef("thread3 P! now c is %d\n",c + 1);
+        writef("sone P! now c is %d\n",c);
     }
 }
 
 void umain() {
-    writef("umain thread\n");
-    u_int a[1];
+    u_int a[2];
+    a[0] = syscall_getthreadid();
     pthread_t thread1;
     pthread_t thread2;
     pthread_t thread3;
     sem_t mysem;
     sem_init(&mysem,0,1);
-    a[0] = &mysem;
+    a[1] = &mysem;
     int r;
-    pthread_create(&thread1,NULL,run1,(void *)a);
-    pthread_create(&thread2,NULL,run2,(void *)a);
-    pthread_create(&thread3,NULL,run3,(void *)a);
+    pthread_create(&thread1,NULL,test1,(void *)a);
+    pthread_create(&thread2,NULL,test2,(void *)a);
+    pthread_create(&thread3,NULL,test3,(void *)a);
     int value;
     r = sem_getvalue(&mysem,&value);
     if (r < 0) {
@@ -67,7 +63,7 @@ void umain() {
     int i = 0;
     for (i = 0; i < 9; ++i) {
         if (value == 0) {
-            writef("umain thread post the %d!\n", i+1);
+            writef("father post!\n");
             sem_post(&mysem);
         }
         syscall_yield();
